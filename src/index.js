5b37d240e716f563e1e01e06a3f08879ccca7d61
Marko Matic <mail.marko.matic@gmail.com>
2020-09-27 11:26:51 +1000

    - Allow rendering actions

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import { Surface, Caption } from 'react-native-paper';
+import map from 'lodash/map';
+import { Surface, Caption, Button } from 'react-native-paper';
 import { AntDesign } from '@expo/vector-icons';
 
 export const InfoPanelRoot = styled(Surface)`
     display: flex;
-    flex-direction: row;
-    align-items: center;
+    flex-direction: column;
     elevation: 2;
     margin: 2px;
     padding: 5px;
     background-color: ${({ theme: { colors: { background } } }) => background};
+`;
+
+export const InfoPanelMessage = styled.View`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
 `;
 
 export const InfoPanelIcon = styled(AntDesign)`
@@ -22,15 +28,43 @@ export const InfoPanelText = styled(Caption)`
     flex: 1;
 `;
 
+export const InfoPanelActions = styled.View`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-end;
+    flex-wrap: wrap;
+    margin-top: 5px;
+`;
+
+const renderActions = actions => actions && (
+    <InfoPanelActions>
+        {map(actions, ({ text, action }) => (
+            <Button
+                key={text}
+                mode='text'
+                onPress={action}
+                labelStyle={{ fontSize: 12 }}
+            >
+                {text}
+            </Button>
+        ))}
+    </InfoPanelActions>
+);
+
 export const InfoPanel = ({
     icon,
-    text
+    text,
+    actions
 }) => (
     <InfoPanelRoot>
-        <InfoPanelIcon
-            name={icon}
-            size={24}
-        />
-        <InfoPanelText>{text}</InfoPanelText>
+        <InfoPanelMessage>
+            <InfoPanelIcon
+                name={icon}
+                size={24}
+            />
+            <InfoPanelText>{text}</InfoPanelText>
+        </InfoPanelMessage>
+        {renderActions(actions)}
     </InfoPanelRoot>
 );
